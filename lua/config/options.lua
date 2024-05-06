ba62a7a2d779a055f39ef8642275b80332f9953d
unknown <kxinhao@gmail.com>
2024-05-06 16:58:56 +0800

    removed animation, added tab 4 spaces

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,3 +2,7 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.opt.list = false
+vim.o.tabstop = 4 -- tab char uses 4 spaces
+vim.o.expandtab = true -- tab uses spaces instead of tab char
+vim.o.softtabstop = 4 -- number of spaces replacing tab char
+vim.o.shiftwidth = 4 -- number of spaces inserted when indenting
